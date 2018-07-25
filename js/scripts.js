689174577e8ee61c792c4aa7b994767c25bf7332
teabag98 <eric.kingoo2014@gmail.com>
2018-07-25 11:59:48 +0300

    js changes

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,88 @@ function myFunction() {
         x.style.display = "none";
     }
 }
+
+// user logic
+$(document).ready(function(){
+  $(".p1").hide()
+  $("#button-home").click(function(event){
+  $(".p1").show();
+
+
+
+
+
+});
+});
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+
+
+        $().UItop({ easingType: 'easeOutQuart' });
+
+    });
+</script>
+<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+=======
+
+/* Highlight the current section in the navigation bar
+------------------------------------------------------*/
+
+	var sections = $("section");
+	var navigation_links = $("#nav-wrap a");
+
+	sections.waypoint({
+
+      handler: function(event, direction) {
+
+		   var active_section;
+
+			active_section = $(this);
+			if (direction === "up") active_section = active_section.prev();
+
+			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+         navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+
+		},
+		offset: '35%'
+
+	});
+
+
+
+  /*	Fade In/Out Primary Navigation
+------------------------------------------------------*/
+
+   $(window).on('scroll', function() {
+
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+      var nav = $('#nav-wrap');
+
+	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+	      nav.fadeOut('fast');
+	   }
+      else {
+         if (y < h*.20) {
+            nav.removeClass('opaque').fadeIn('fast');
+         }
+         else {
+            nav.addClass('opaque').fadeIn('fast');
+         }
+      }
+
+	});
+
+function myFunction() {
+    var x = document.getElementById("fullscreen_bg");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
